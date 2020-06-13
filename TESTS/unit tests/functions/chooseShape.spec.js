a2fa2a2ea2463b3e9e8801e9945f508f86cb662b
miaklwalker <ouisihai@gmail.com>
2020-06-13 11:59:05 -0500

    Adding more unit Tests!

--- TESTS/unit tests/functions/chooseShape.spec.js
+++ TESTS/unit tests/functions/chooseShape.spec.js
@@ -2,60 +2,27 @@ import {describe,expect,test,jest} from "@jest/globals";
 import chooseShape from "../../../src/helperFunctions/chooseShape.js";
 
 let arrs = [
-    [false ,false ,false ,false],
-    [true  ,false ,false ,false],
-    [false ,true  ,false ,false],
-    [false ,false ,true  ,true ],
-    [false ,false ,true ,false ],
-    [true  ,false ,true  ,false],
-    [true  ,false ,false ,true ],
-    [true  ,false ,true  ,true ],
-    [false ,false ,false ,true ],
-    [false ,true  ,true  ,false],
-    [false ,true  ,false ,true ],
-    [false ,true  ,true  ,true ],
-    [true  ,true  ,false ,false],
-    [true  ,true  ,true  ,false],
-    [true  ,true  ,false ,true ],
-    [true  ,true  ,true  ,true ]
+    [[false ,false ,false ,false] ,'single'],
+    [[true  ,false ,false ,false] , 'right'],
+    [[false ,true  ,false ,false] , 'top' ],
+    [[false ,false ,true  ,true ] , 'bottomLeft'],
+    [[false ,false ,true ,false ] , 'left'],
+    [[true  ,false ,true  ,false] , 'middle'],
+    [[true  ,false ,false ,true ] , 'bottomRight'],
+    [[true  ,false ,true  ,true ] , 'middleJoint'],
+    [[false ,false ,false ,true ] , 'bottom'],
+    [[false ,true  ,true  ,false] , 'topLeft'],
+    [[false ,true  ,false ,true ] , 'verticalMiddle'],
+    [[false ,true  ,true  ,true ] , 'verticalMiddleRight'],
+    [[true  ,true  ,false ,false] , 'topRight'],
+    [[true  ,true  ,true  ,false] , 'middleDown'],
+    [[true  ,true  ,false ,true ] , 'verticalMiddleLeft'],
+    [[true  ,true  ,true  ,true ] , 'fourWay']
 ];
-let result=[
-    'single',
-    'right',
-    'top',
-    'bottomLeft',
-    'left',
-    'middle',
-    'bottomRight',
-    'middleJoint',
-    'bottom',
-    'topLeft',
-    'verticalMiddle',
-    'verticalMiddleRight',
-    'topRight',
-    'middleDown',
-    'verticalMiddleLeft',
-    'fourWay'
 
-];
     describe(`Choose Shape`,()=> {
-        test('Should Choose the correct Shape',()=>{
-            expect(chooseShape(arrs[0])).toBe(result[0]);
-            expect(chooseShape(arrs[1])).toBe(result[1]);
-            expect(chooseShape(arrs[2])).toBe(result[2]);
-            expect(chooseShape(arrs[3])).toBe(result[3]);
-            expect(chooseShape(arrs[4])).toBe(result[4]);
-            expect(chooseShape(arrs[5])).toBe(result[5]);
-            expect(chooseShape(arrs[6])).toBe(result[6]);
-            expect(chooseShape(arrs[7])).toBe(result[7]);
-            expect(chooseShape(arrs[8])).toBe(result[8]);
-            expect(chooseShape(arrs[9])).toBe(result[9]);
-            expect(chooseShape(arrs[10])).toBe(result[10]);
-            expect(chooseShape(arrs[11])).toBe(result[11]);
-            expect(chooseShape(arrs[12])).toBe(result[12]);
-            expect(chooseShape(arrs[13])).toBe(result[13]);
-            expect(chooseShape(arrs[14])).toBe(result[14]);
-            expect(chooseShape(arrs[15])).toBe(result[15]);
+        test.each(arrs)('When Given %p expect to receive %p',(input,expected)=>{
+            expect(chooseShape(input)).toBe(expected);
         })
     });
 
--- TESTS/unit tests/functions/getColorCordinates.spec.js
+++ TESTS/unit tests/functions/getColorCordinates.spec.js
@@ -4,35 +4,20 @@ import getColorCoords from "../../../src/helperFunctions/getColorCoords.js";
 
 
 describe('Get Color Coordinates', () => {
-    let inputs = [
-        [488, 24, 0],
-        [488, 24, 1],
-        [488, 24, 6],
-        [488, 24, 7],
-        [488, 24, 14],
-        [488, 24, 21],
-        [488, 24, 28],
-        [488, 24, 34]
+    let testData = [
+        [[488, 24, 0], [489, 25, 6, 6]],
+        [[488, 24, 1], [497, 25, 6, 6]],
+        [[488, 24, 6], [537, 25, 6, 6]],
+        [[488, 24, 7], [489, 33, 6, 6]],
+        [[488, 24, 14],[489, 41, 6, 6]],
+        [[488, 24, 21],[489, 49, 6, 6]],
+        [[488, 24, 28],[489, 57, 6, 6]],
+        [[488, 24, 34],[537, 57, 6, 6]],
 
     ];
-    let outputs = [
-        [489, 25, 6, 6],
-        [497, 25, 6, 6],
-        [537, 25, 6, 6],
-        [489, 33, 6, 6],
-        [489, 41, 6, 6],
-        [489, 49, 6, 6],
-        [489, 57, 6, 6],
-        [537, 57, 6, 6]
 
-
-    ];
-    test('Should Return the correct array', () => {
-        let mockColorCord = jest.fn(getColorCoords);
-        inputs.forEach(([x, y, tint], index) => {
-            mockColorCord(x, y, tint);
-            expect(mockColorCord).toHaveReturnedWith(outputs[index]);
-        })
-    })
+    test.each(testData)('getColorCoords when passed %p should return %p',(input, expected)=>{
+        expect(getColorCoords(...input)).toEqual(expect.arrayContaining(expected));
+    });
 })
 
